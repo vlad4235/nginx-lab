8d63ad9026d722adacb7233609056c173d15075b
Константинов Владислав <vlad_konstantinov_2017@list.ru>
2025-10-22 19:53:14 +0200

    Интеграция Lab4: Добавление Composer в Lab3
    - Создан composer.json с зависимостью GuzzleHTTP
    - Добавлен vendor/autoload.php для автозагрузки классов
    - Настроена структура проекта Composer
    - Конфигурирована PSR-4 автозагрузка

---
+++ vendor/autoload.php
@@ -0,0 +1,18 @@
+﻿<?php
+// Простой автозагрузчик для проекта
+spl_autoload_register(function ($class_name) {
+    $file = __DIR__ . '/../www/' . $class_name . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+// Подключаем наши классы
+$project_files = ['ApiClient', 'UserInfo'];
+foreach ($project_files as $file) {
+    $file_path = __DIR__ . '/../www/' . $file . '.php';
+    if (file_exists($file_path)) {
+        require_once $file_path;
+    }
+}
+?>
